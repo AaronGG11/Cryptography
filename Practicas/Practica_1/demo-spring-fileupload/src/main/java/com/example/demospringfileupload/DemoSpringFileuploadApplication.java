6ed6e4822aca6f38bba0724d216089044ab0b8aa
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-10-27 22:22:44 -0600

    Creando formularios index, cifrar y descifrar para cada algoritmo, sin funcionalidad

--- Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/DemoSpringFileuploadApplication.java
+++ Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/DemoSpringFileuploadApplication.java
@@ -1,20 +1,13 @@
 package com.example.demospringfileupload;
 
-import com.example.demospringfileupload.crypto.Vigenere;
-import com.example.demospringfileupload.crypto.Affine;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.demospringfileupload.crypto.modularArithmetic.AritmeticaModular;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class DemoSpringFileuploadApplication
 {
 	public static void main(String[] args)
 	{
-		//SpringApplication.run(DemoSpringFileuploadApplication.class, args);
-		String plain_text = "hola mundo";
-		String cipher_text = Affine.Encrypt(plain_text, 256, 3, 5);
-		String decipher_text = Affine.Decrypt(cipher_text, 256, 3, 5);
-		System.out.println(decipher_text);
+		SpringApplication.run(DemoSpringFileuploadApplication.class, args);
 	}
 }
