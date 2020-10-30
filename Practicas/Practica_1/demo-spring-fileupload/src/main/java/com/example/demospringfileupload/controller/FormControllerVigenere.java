99ef698b4027449215ddb1c22777782847f023ab
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-10-29 18:59:38 -0600

    Agrgando parcialmente controller de cifrador vigenere

--- Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/controller/FormControllerVigenere.java
+++ Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/controller/FormControllerVigenere.java
@@ -42,17 +42,58 @@ public class FormControllerVigenere {
 
 
 	@PostMapping("/e_upload")
-	public String uploadFileE(@ModelAttribute("vigenere") ModelVigenereEncrypt vigenere) throws IOException {
+	public String uploadFileE(@ModelAttribute("vigenere") ModelVigenereEncrypt vigenere, RedirectAttributes attributes) throws IOException {
+		ModelVigenereEncrypt aux = new ModelVigenereEncrypt();
 
-		String clave = new String(vigenere.getClave().getBytes());
-		int longitud_alfabeto = vigenere.getLongitud_alfabeto();
-		String originalString = new String(vigenere.getArchivo().getBytes());
+		aux.setArchivo(vigenere.getArchivo());
+		aux.setClave(vigenere.getClave());
+		aux.setLongitud_alfabeto(vigenere.getLongitud_alfabeto());
+		aux.setEsAleatorio(vigenere.isEsAleatorio());
+
+		// El archivo no válido
+		if (aux.getArchivo().getBytes().toString() == null || aux.getArchivo().getBytes().toString().isEmpty()) {
+			attributes.addFlashAttribute("message", "Por favor seleccione un archivo válido.");
+			return "redirect:status";
+		} // Clave no válida, porque es no aleatoria y es vacia o no se lleno el campo
+		else if (!aux.isEsAleatorio() && (aux.getClave().isEmpty() || (aux.getClave().length() <= 0))) {
+			attributes.addFlashAttribute("message", "Clave no válida.");
+			return "redirect:status";
+		} // Alfabeto no válido, porque es de longitud 0 o menor
+		else if(aux.getLongitud_alfabeto() <= 0){
+			attributes.addFlashAttribute("message", "Alfabeto no válido.");
+			return "redirect:status";
+		} // Los argumentos son correctos
+		else{
+			// Making path
+			StringBuilder builder = new StringBuilder();
+			builder.append(System.getProperty("user.home"));
+			builder.append(File.separator);
+			builder.append("Ejemplos");
+			builder.append(File.separator);
+			builder.append(aux.getArchivo().getOriginalFilename());
+			builder.append(".vig");
+
+			// Section encryption
+
+			// Verificar si es llave aleatoria y no
+			if(aux.isEsAleatorio()){
+
+			}
+			else {
+				final String secretKey = aux.getClave();
+			}
+			
+			String originalString = new String(file.getBytes());
+			String encryptedString = AES.encrypt(originalString, secretKey);
 
 
-		System.out.println(originalString);
-		System.out.println(clave);
-		System.out.println(longitud_alfabeto);
-		System.out.println(vigenere.isEsAleatorio());
+
+		}
+
+
+
+
+
 
 		return "redirect:/status";
 	}
