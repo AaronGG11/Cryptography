fffe5b569d8c83cf7a59805dcc61be667ea990f5
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-10-30 23:01:36 -0600

    Creando formularios para Affine con Thymeleaf

--- Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/DemoSpringFileuploadApplication.java
+++ Practicas/Practica_1/demo-spring-fileupload/src/main/java/com/example/demospringfileupload/DemoSpringFileuploadApplication.java
@@ -1,6 +1,5 @@
 package com.example.demospringfileupload;
 
-import com.example.demospringfileupload.crypto.Vigenere;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,16 +9,5 @@ public class DemoSpringFileuploadApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DemoSpringFileuploadApplication.class, args);
-		/*
-		String plain_text = "mi nombre es aaron";
-		String key = "garcia";
-
-		String cipher_text = Vigenere.Encrypt(plain_text,key,26);
-		String decipher_text = Vigenere.Decrypt(cipher_text,key,26);
-
-		System.out.println(cipher_text);
-		System.out.println(decipher_text);
-		System.out.println(plain_text);
-		* */
 	}
 }
